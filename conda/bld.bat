e37cc3029f32fec703cb5678d36910da42a5480c
Giuseppe Le Voci <g.levoci@bopen.eu>
2024-01-19 15:30:43 +0000

    pip installs via proper Python

--- conda/bld.bat
+++ conda/bld.bat
@@ -85,7 +85,7 @@ call gen\install.bat fcicomp-H5Zjpegls
 if errorlevel 1 exit 1
 
 cd %FCIDECOMP_BUILD_PATH%
-call %PREFIX%\Scripts\pip install --no-deps --ignore-installed -vv ../fcidecomp-python
+call python -m %PREFIX%\Scripts\pip install --no-deps --ignore-installed -vv ../fcidecomp-python
 
 if not exist %PREFIX%\etc\conda\activate.d mkdir %PREFIX%\etc\conda\activate.d
 copy %RECIPE_DIR%\scripts\activate.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
